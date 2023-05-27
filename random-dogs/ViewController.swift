694b88756b177d2c8300e67e2c41146755c1c9eb
jisulee154 <jisulee154@gmail.com>
2023-05-27 20:13:31 +0900

    initial commit

---
+++ random-dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random-dogs
+//
+//  Created by 이지수 on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
