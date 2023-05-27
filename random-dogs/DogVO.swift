d1062d76263f060b49bf9a4fa392744e8a46de5d
jisulee154 <jisulee154@gmail.com>
2023-05-27 21:55:30 +0900

    Get a dog info using API

---
+++ random-dogs/DogVO.swift
@@ -0,0 +1,19 @@
+//
+//  DogVO.swift
+//  random-dogs
+//
+//  Created by 이지수 on 2023/05/27.
+//
+
+import Foundation
+
+class DogVO {
+    var thumbnail: String?
+    var id: String?
+    var breed: String?
+    var date: String?
+    var none: String?
+    //var rating: Double?
+    //var thumbnail: String?
+    
+}
---
+++ random-dogs/InfoCell.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCell.swift
+//  random-dogs
+//
+//  Created by 이지수 on 2023/05/27.
+//
+
+import UIKit
+
+class InfoCell:UITableViewCell {
+    @IBOutlet var id: UILabel!
+    @IBOutlet var breed: UILabel!
+    @IBOutlet var date: UILabel!
+    @IBOutlet var none: UILabel!
+    @IBOutlet var thumbnail: UIImageView!
+    
+}
