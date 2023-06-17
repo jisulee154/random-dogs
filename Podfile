b0b3a845c1255b1d6b2365af2e48e0d9677bae52
jisulee154 <jisulee154@gmail.com>
2023-06-18 00:18:22 +0900

    protocol을 활용한 view controller간 데이터 전송

--- Podfile
+++ Podfile
@@ -7,9 +7,10 @@ target 'random-dogs' do
 
   # Pods for random-dogs
 
-  #pod 'RealmSwift', '~>10'
+  # pod 'RealmSwift', '~>10'
   pod 'Alamofire'
   pod 'Tabman', '~> 3.0'
+  pod 'Toast-Swift', '~> 5.0.0'
   
   target 'random-dogsTests' do
     inherit! :search_paths
--- random-dogs/Control/SecondTabViewController.swift
+++ random-dogs/Control/SecondTabViewController.swift
@@ -9,5 +9,61 @@ import Foundation
 import Tabman
 
 class SecondTabViewController: TabmanViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    var collectedDogImages = [UIImage]()
+    let sectionInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    
+    override func viewDidLoad() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+//MARK: - CollectionView
+extension SecondTabViewController: UICollectionViewDataSource,
+                                   UICollectionViewDelegate,
+                                   UICollectionViewDelegateFlowLayout
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectedDogImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let imageData = collectedDogImages[indexPath.row]
+        let appendingView = UIView(frame: CGRect.init())
+        let appendingImageView = UIImageView(image: imageData)
+        appendingView.addSubview(appendingImageView)
+        
+        cell.contentView.addSubview(appendingView)
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = collectionView.frame.height
+        let itemsPerRow: CGFloat = 2
+        let widthPadding = sectionInsets.left * (itemsPerRow + 1)
+        let itemsPerColumn: CGFloat = 3
+        let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
+        let cellWidth = (width - widthPadding) / itemsPerRow
+        let cellHeight = (height - heightPadding) / itemsPerColumn
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+}
+
+//MARK: - InsertImageDelegate
+extension SecondTabViewController: InsertImageDelegate {
+    func insertImage(with image: UIImage) {
+        
+        self.collectedDogImages.append(image)
+        let itemLocation = self.collectedDogImages.count - 1
+        let indexPaths = IndexPath(item: itemLocation, section: 0)
+        
+        self.collectionView.insertItems(at: [indexPaths])
+    }
 }
